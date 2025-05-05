3b261a5ae7137e9dc00392e41574dd3d30dbc2dd
stellachy <chy.stella.chuang@gmail.com>
2025-05-05 14:39:00 +0800

    調整語系檔＋完成語言、dark mode的toggle

--- main.js
+++ main.js
@@ -6,9 +6,25 @@ themeToggle.addEventListener('change', () => {
   body.classList.toggle('dark-mode', themeToggle.checked);
 });
 
+// 預設語言（抓取使用者預設系統！）
+let currentLang  = 'en';  // 預設是英文
+
+let userLang = navigator.language || navigator.userLanguage;
+
+userLang = userLang.toLowerCase();
+if (userLang.startsWith('zh')) {
+  currentLang = 'zh';
+}
+
+const supportedLangs = ['en', 'zh'];
+if (!supportedLangs.includes(currentLang)) {
+  currentLang = 'en';
+}
+
+loadLanguage(currentLang);  // 初始載入語言
+
 // Language Toggle
 const langToggle = document.getElementById('lang-toggle');
-let currentLang  = 'en';  // 預設是英文
 
 langToggle.addEventListener('click', () => {
   currentLang = currentLang === 'en' ? 'zh' : 'en';
@@ -16,10 +32,25 @@ langToggle.addEventListener('click', () => {
   loadLanguage(currentLang);
 });
 
-async function loadLanguage(lang) {
-  const response = await fetch(`lang/${lang}.json`);
-  const data = await response.json();
+langToggle.textContent = currentLang === 'en' ? '中文' : 'EN';  // 初始調整toggle文字
 
+async function loadLanguage(lang) {
+  try {
+    const response = await fetch(`lang/${lang}.json`);
+
+    if (!response.ok) {
+      throw new Error(`Could not load language file: ${lang}.json`);
+    }
+
+    const data = await response.json();
+    updateTexts(data);
+    
+  } catch(error) {
+    console.error('Language loadinf error', error);
+  }
+}
+
+function updateTexts(data) {
   document.querySelectorAll('[data-i18n]').forEach(elem => {
     const key = elem.getAttribute('data-i18n');
     const keys = key.split('.'); // 比如 "nav.about" => ["nav", "about"]
@@ -32,6 +63,5 @@ async function loadLanguage(lang) {
     if (value) {
       elem.textContent = value;
     }
-  })
+  });
 }
-
